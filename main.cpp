73068a3612780dada407eb206585294b9b61146d
241199 <241199@edu.p.lodz.pl>
2022-01-29 17:09:56 +0100

    zmiana na mnożenie w metodzie policz

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-int policz (int a, int b) {
-    return a*b;
-}
+int policz(int a, int b){
+    return a+b;
 
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "HW dpierwszym branchu; 2*3=" << policz(2,3) << std::endl;
     return 0;
 }
